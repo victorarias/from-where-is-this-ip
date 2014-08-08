a6ef77a6eb307617ca68dbe64282f980f4addc00
Victor Arias <arias.sp@gmail.com>
2014-08-08 17:19:08 +0200

    Use PUT instead of GET to receive IP addresses

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var geoip = require('geoip-lite');
 var express = require("express");
+var bodyParser = require('body-parser')
+
 var app = express();
 var token = process.env.TOKEN || "dev";
 
-app.get("/convert", function(req, res) {
+app.use(bodyParser.urlencoded());
+
+app.put("/convert", function(req, res) {
   if(req.query.token != token) {
     return res.status(403).end();
   }
 
   var result = {}
-  var ips = req.query.ips.split(",");
+  var ips = req.body.ips.split(",");
   ips.forEach(function(ip) {
     result[ip] = geoip.lookup(ip);
   });
